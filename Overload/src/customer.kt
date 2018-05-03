87111d4887c7c15a631d16285bbcfa1689cf9959
MoonOnYou <onyou0031@naver.com>
2018-05-03 19:09:35 +0900

    범위연산자와 in연산자를 이용하여 날짜범위를 사용하는 예

--- Overload/src/customer.kt
+++ Overload/src/customer.kt
@@ -1,8 +1,16 @@
+import java.time.LocalDate
+
 fun main(args: Array<String>) {
     val p1 = Customer("홍길동", "010-1234-5678")
     val p2 = Customer("김선달", "010-5678-1234")
     println(p1 < p2)
     println(p1 > p2)
+    println()
+
+    val start = LocalDate.now()
+    val end = start..start.plusDays(15)
+    println(start.plusWeeks(1) in end)
+    println(end)
 }
 
 class Customer(val name: String, val phone: String) : Comparable<Customer> {
