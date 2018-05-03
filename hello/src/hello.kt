1629f2ca35b2ef11cd7c2522a9e3b1775c5faf86
MoonOnYou <onyou0031@naver.com>
2018-05-03 17:37:11 +0900

    생산적인 언어가 고객의 요구사항을 더 빠르게 잘 반영할 수 있다 들었다. 그래서 난 자바대신 코틀린을 사용하기로 했다

---
+++ hello/src/hello.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>){
+    printHello(3)
+}
+fun printHello(msgType: Int){
+    when(msgType){
+        1, 2 -> println("안녕하세요")
+        else -> println("메세지 번호 오류")
+    }
+}                       
