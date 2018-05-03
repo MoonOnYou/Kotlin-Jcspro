d5ab82d9641f39f9da6e85aa2f06f1d6fc15775b
MoonOnYou <onyou0031@naver.com>
2018-05-03 18:51:15 +0900

    고객의 이름과 전화번호를 간단히 비교하는 예제

---
+++ Overload/src/customer.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    val p1 = Customer("홍길동", "010-1234-5678")
+    val p2 = Customer("김선달", "010-5678-1234")
+    println(p1 < p2)
+    println(p1 > p2)
+}
+
+class Customer(val name: String, val phone: String) : Comparable<Customer> {
+    override fun compareTo(other: Customer): Int {
+        return compareValuesBy(this, other, Customer::name, Customer::phone)
+    }
+}
