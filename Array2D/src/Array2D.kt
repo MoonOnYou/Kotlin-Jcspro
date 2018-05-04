87363a59b773e7460c28edf056cc25ff2a3951cd
MoonOnYou <onyou0031@naver.com>
2018-05-04 20:23:25 +0900

    continue와 break, break@라벨

--- Array2D/src/Array2D.kt
+++ Array2D/src/Array2D.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    val result = createArray(5,5)
+    val result = createArray(5,9)
     printArray(result)
 }
 
@@ -7,9 +7,18 @@ fun createArray (row: Int, col: Int): Array<Array<Int>>{
     val newArray = Array(row, { Array(col, { i -> 0 })})
 
     var rowValue = 1
-    for (x in 0 until row - 1) {
-        for(y in 0 until col - 1) {
+    end@ for (x in 0 .. row - 1) {
+        for(y in 0 .. col - 1) {
+
+            if(rowValue >= 40)
+                break@end
+
             newArray[x][y] = rowValue + y
+
+            if ((rowValue + y) % 2 != 0)            //2. if문이 ture일 경우
+                continue                            //1. continue가 있으므로
+            newArray[x][y] *= 10                    //3. 이 문장을 실행하지않고 다음 for문을 실행
+
         }
         rowValue += 10
     }
@@ -17,8 +26,8 @@ fun createArray (row: Int, col: Int): Array<Array<Int>>{
 }
 
 fun printArray(arr: Array<Array<Int>>){
-    for(x in 0 until  arr.size - 1) {
-        for(y in 0 until arr[x].size - 1) {
+    for(x in 0 ..  arr.size - 1) {
+        for(y in 0 .. arr[x].size - 1) {
             print("${arr[x][y]} ")
         }
         println()
