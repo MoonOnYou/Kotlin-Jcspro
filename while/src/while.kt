74706a7f307dbe0d40e4c03138b8840fb9d37b7e
MoonOnYou <onyou0031@naver.com>
2018-05-04 19:36:04 +0900

    while예제 실습중 버그발견과 수정

--- while/src/while.kt
+++ while/src/while.kt
@@ -1,13 +1,16 @@
 fun main(args: Array<String>) {
-    val item = listOf("사과", "바나나", "키위")
+    val items = listOf("사과", "바나나", "키위")
     var index = 0
-    while (index < item.size) {
-        println("item at $index is ${item[index]}")
+    while (index < items.size) {
+        println("item at $index is ${items[index]}")
         index++
     }
 
+    println()
+
     index = 0
     do {
-        println("item at $index is ${item[index]}")
-    } while (index < item.size)
+        println("item at $index is ${items[index]}")
+        index++
+    } while (index < items.size)
 }
